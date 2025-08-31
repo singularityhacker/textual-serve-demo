7e0d41e14623f01de3b3635d6b2909342ac26374
0xJustice.eth <justiceconder@gmail.com>
2025-08-31 17:38:54 -0400

    setting a public url

--- railway_server.py
+++ railway_server.py
@@ -2,5 +2,10 @@ import os
 from textual_serve.server import Server
 
 port = int(os.environ.get('PORT', 8000))
-server = Server("python app.py", port=port, host="0.0.0.0")
+server = Server(
+    "python app.py", 
+    port=port, 
+    host="0.0.0.0",
+    public_url="https://textual-serve-demo-production.up.railway.app/"
+)
 server.serve()
